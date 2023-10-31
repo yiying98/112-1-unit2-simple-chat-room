5ec9f1ff1dd8b448fe08cffad90dea1d6c41fb62
ba144220 <hs41511@gmail.com>
2023-10-31 21:26:14 +0800

    fix: remove default data

--- frontend/src/app/db/index.ts
+++ frontend/src/app/db/index.ts
@@ -1,10 +1,4 @@
 import { Message } from "@/lib/types/message";
 export const db: { messages: Message[] } = {
-  messages: [
-    {
-      content: "Hello World",
-      senderId: "yuchi",
-      timestamp: new Date(),
-    },
-  ],
+  messages: [],
 };
